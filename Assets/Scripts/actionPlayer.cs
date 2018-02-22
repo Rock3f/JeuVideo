9174dd02d3072512fd34e5a463f9d90dd17d0423
Rock3f <tanguy.badier@ynov.com>
2018-02-22 09:15:56 +0100

    Add basic scripts

---
+++ Assets/Scripts/actionPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actionPlayer : MonoBehaviour {
+
+	private animationSprite anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<animationSprite> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)) {
+			anim.SetAction("attack1");
+		}
+	}
+}
