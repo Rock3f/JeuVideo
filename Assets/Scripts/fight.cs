6f9eddcafb8e7e6465d4ab54c9dea7d4a7827bc5
AntoineGOSSET <antoine.gosset@ynov.com>
2018-03-07 21:00:35 +0100

    ajout animation lors de prise de degat + animation mort

--- Assets/Scripts/fight.cs
+++ Assets/Scripts/fight.cs
@@ -28,7 +28,7 @@ public class fight : MonoBehaviour {
 
 	private string EnemyType;
 
-	private string CollisonSide;
+	public string CollisonSide;
 
 	private float topAngle;
     private float sideAngle;
@@ -48,16 +48,32 @@ public class fight : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		//Est detruit quand sa vie arrive à 0
-		if (hp <= 0){
-			DestroyImmediate(this.gameObject);
-		}
+		
 		// Initialise pour detection d'angle
 		Vector2 size = GetComponent<BoxCollider2D>().size;
          size = Vector2.Scale (size, (Vector2)transform.localScale);
          topAngle = Mathf.Atan (size.x / size.y) * Mathf.Rad2Deg;
          sideAngle = 90.0f - topAngle;
-         Debug.Log (topAngle + ", " + sideAngle);
+	}
+
+	void LateUpdate() {
+		// Lorsque la vie arrive à zero fait l'animation de mort
+		if(hp <= 0){
+			if(gameObject.GetComponent<animationSprite>().currentAnim.name != "die")
+			{
+				gameObject.GetComponent<animationSprite>().ChangeAnimation("die", true);
+			}
+			// Le dernier sprite de l'animation est vide et fait disparaitre le personnage
+			if(GetComponent<SpriteRenderer>().sprite == null){
+				gameObject.SetActive(false);
+				// Si c'est un mechant il est le GameObject est détruit
+				if (EnemyType == "Player"){
+					DestroyImmediate(this.gameObject);
+				}
+				
+			}	
+		}
+		
 	}
 
     public void OnCollisionStay2D (Collision2D coll)
@@ -86,7 +102,7 @@ public class fight : MonoBehaviour {
 			// Pour chaque attack regarde si le sprite correspond au sprite de degat associé
 			foreach (Attack att in Attacks){
 				if(GetComponent<SpriteRenderer>().sprite == att.SpriteDamage 
-				&& accumulateur > 0.2 
+				&& accumulateur > 0.2
 				// Vérifie que les ennemies sont bien face a face pour se frapper
 				&& ((CollisonSide == "R" && coll.gameObject.GetComponent<fight>().CollisonSide == "L" && GetComponent<SpriteRenderer>().flipX == false) 
 				|| (CollisonSide == "L" && coll.gameObject.GetComponent<fight>().CollisonSide == "R" &&GetComponent<SpriteRenderer>().flipX))
@@ -96,7 +112,10 @@ public class fight : MonoBehaviour {
 					// Inflige des dégats en fonction de l'attaque
 					coll.gameObject.GetComponent<fight>().hp -= att.dammageValue;
 
-					// RAJOUTER : Faire passer à l'animation normale (contres)
+					// Déclenche l'animation hit
+					if (coll.gameObject.GetComponent<fight>().hp > 0){
+						coll.gameObject.GetComponent<animationSprite>().ChangeAnimation("hit", true);
+					}
 
 					// Si le personnage a une barre de combo la rempli en fonction de l'attaque
 					if (UltBarVar != null){
