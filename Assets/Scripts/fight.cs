67c361d7a0299b259ba1372716ada18cf1fc4080
AntoineGOSSET <antoine.gosset@ynov.com>
2018-03-07 19:30:17 +0100

    Gestion angle collision attaque

--- Assets/Scripts/fight.cs
+++ Assets/Scripts/fight.cs
@@ -28,10 +28,15 @@ public class fight : MonoBehaviour {
 
 	private string EnemyType;
 
-	private float angle;
+	private string CollisonSide;
+
+	private float topAngle;
+    private float sideAngle;
 
 	// Use this for initialization
 	void Start () {
+
+		// Determine le type de personnage qu'il peut frapper
 		if (this.gameObject.tag == "Player"){
 			EnemyType = "Enemy";
 		}
@@ -42,30 +47,63 @@ public class fight : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Est detruit quand sa vie arrive à 0
 		if (hp <= 0){
 			DestroyImmediate(this.gameObject);
 		}
+		// Initialise pour detection d'angle
+		Vector2 size = GetComponent<BoxCollider2D>().size;
+         size = Vector2.Scale (size, (Vector2)transform.localScale);
+         topAngle = Mathf.Atan (size.x / size.y) * Mathf.Rad2Deg;
+         sideAngle = 90.0f - topAngle;
+         Debug.Log (topAngle + ", " + sideAngle);
 	}
 
-    public void OnCollisionStay2D (Collision2D col)
+    public void OnCollisionStay2D (Collision2D coll)
     {
-
-        angle = Vector2.SignedAngle(col.gameObject.transform.position, transform.position);
-		
-        if (angle < 5.0f){
-            print("close");
+		// Detecte de quel cote viens la collison
+		Vector2 v = coll.contacts[0].point - (Vector2)transform.position;
+ 
+		if (Vector2.Angle(v, transform.up) <= topAngle) {
+			CollisonSide = "T";
+		}
+		else if (Vector2.Angle(v, transform.right) <= sideAngle)  {
+			CollisonSide = "R";
+		}
+		else if (Vector2.Angle(v, -transform.right) <= sideAngle) {
+			CollisonSide = "L";
+		}
+		else {
+			CollisonSide = "B";
 		}
 
-
+		// Permet de n'infliger qu'une seul fois des dégats par coup
         accumulateur += Time.deltaTime;
-		foreach (Attack att in Attacks){
-			if(col.gameObject.tag == EnemyType && this.GetComponent<SpriteRenderer>().sprite == att.SpriteDamage && accumulateur > 0.2)
-			{
-				col.gameObject.GetComponent<fight>().hp -= att.dammageValue;
-				if (UltBarVar != null){
-					UltBarVar.GetComponent<UltBar>().hit += att.comboValue;
+
+		// Lorsqu'une collison à lieu avec un enemie
+		if(coll.gameObject.tag == EnemyType){
+			// Pour chaque attack regarde si le sprite correspond au sprite de degat associé
+			foreach (Attack att in Attacks){
+				if(GetComponent<SpriteRenderer>().sprite == att.SpriteDamage 
+				&& accumulateur > 0.2 
+				// Vérifie que les ennemies sont bien face a face pour se frapper
+				&& ((CollisonSide == "R" && coll.gameObject.GetComponent<fight>().CollisonSide == "L" && GetComponent<SpriteRenderer>().flipX == false) 
+				|| (CollisonSide == "L" && coll.gameObject.GetComponent<fight>().CollisonSide == "R" &&GetComponent<SpriteRenderer>().flipX))
+				)
+
+				{	
+					// Inflige des dégats en fonction de l'attaque
+					coll.gameObject.GetComponent<fight>().hp -= att.dammageValue;
+
+					// RAJOUTER : Faire passer à l'animation normale (contres)
+
+					// Si le personnage a une barre de combo la rempli en fonction de l'attaque
+					if (UltBarVar != null){
+						UltBarVar.GetComponent<UltBar>().hit += att.comboValue;
+					}
+					accumulateur = 0;
 				}
-				accumulateur = 0;
 			}
 		}
     }
