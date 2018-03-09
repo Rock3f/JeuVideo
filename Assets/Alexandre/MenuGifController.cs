287250c7bec0df81ade2e42ad523a63d8f2fd333
AlexDesvallees <alex.desvallees@gmail.com>
2018-03-09 15:19:46 +0100

    Commentaire dans le code animation gif menu principal

--- Assets/Alexandre/MenuGifController.cs
+++ Assets/Alexandre/MenuGifController.cs
@@ -15,6 +15,8 @@ public class MenuGifController : MonoBehaviour {
     }
  
     private void ChangeImage() {
+		// We comment the 2 lines above to have a loop on our GIF
+		// If we wants to play it once do the opposite of what is comment in the if condition
         if (currentImage == frames.Length - 1) {
             // CancelInvoke("ChangeImage");
 			currentImage = 0;
