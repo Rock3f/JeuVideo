ea32ccdbc8a412112200f56cd8c1748e935856e6
AlexDesvallees <alex.desvallees@gmail.com>
2018-03-09 15:14:42 +0100

    Ajout de l'animation gif sur le menu principal

---
+++ Assets/Alexandre/MenuGifController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+ 
+public class MenuGifController : MonoBehaviour {
+    public Sprite[] frames;
+    public Image menuBackground;
+    public float frameRate = 0.1f;
+ 
+    private int currentImage;
+    // Use this for initialization
+    void Start () {
+        currentImage = 0;
+        InvokeRepeating ("ChangeImage", 0.1f, frameRate);
+    }
+ 
+    private void ChangeImage() {
+        if (currentImage == frames.Length - 1) {
+            // CancelInvoke("ChangeImage");
+			currentImage = 0;
+            // Destroy (explosion);
+        }
+        currentImage += 1;
+        menuBackground.sprite = frames [currentImage];
+    }
+}
