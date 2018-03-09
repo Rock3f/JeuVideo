e9c8e4b6c8741378168d8aabe91c3cd86aaedc09
Rock3f <tanguy.badier@ynov.com>
2018-03-09 19:07:31 +0100

    fix merge

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Canvas pauseCanvas;
     public Scene startScene;
+    public EventSystem eventSystem;
+    public GameObject selectedGameObject;
+    private bool buttonSelected;
+    void Update() {
+        if(Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            eventSystem.SetSelectedGameObject(selectedGameObject);
+            buttonSelected = true;
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            eventSystem.currentSelectedGameObject.GetComponent<Button>().onClick.Invoke();
+        }
+    }
+
+    private void OnDisable() {
+        buttonSelected = false;
+    }
 
     public void PlayGame()
     {
