c1114425ac29a53f2d14cae76a5469a00ece744b
AlexDesvallees <alex.desvallees@gmail.com>
2018-03-16 18:43:08 +0100

    Ajout du Tireur + Correctifs divers

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class EnemyFollow : MonoBehaviour {
 
 	public float speed;
+	public float attackRange;
 	private GameObject[] PlayerList;
 	private Transform Player1;
 
@@ -16,6 +17,9 @@ public class EnemyFollow : MonoBehaviour {
 	private float normalspeed;
 
 	private bool IsAlive;
+
+	private string spritesLastName;
+	private string spriteCurrentName;
 
 	private string attackChoice;
 
@@ -37,27 +41,68 @@ public class EnemyFollow : MonoBehaviour {
 		
 		if(IsAlive)
 		{
-			if (Vector3.Distance(Player1.GetComponent<Transform>().position, transform.position) > Vector3.Distance(Player2.GetComponent<Transform>().position, transform.position)){
-				target = Player2.GetComponent<Transform>();
-			}
-			else{
-				target = Player1.GetComponent<Transform>();
-			}
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x +1, target.position.y, target.position.z), speed * Time.deltaTime);
+			switch (this.gameObject.tag)
+			{
+				// Pour le cas où notre objet est un tireur
+				case("Shooter"):
+					// On défini le target de notre tireur non pas sur la distance mais les hp des joueurs
+					if (Player2.gameObject.GetComponent<fight>().hp < Player1.gameObject.GetComponent<fight>().hp){
+						target = Player2.GetComponent<Transform>();
+					}
+					else{
+						target = Player1.GetComponent<Transform>();
+					}
 
-			gameObject.GetComponent<animationSprite>().SetAnimationFromSpeed(speed, true);
+					gameObject.GetComponent<animationSprite>().SetAnimationFromSpeed(speed, true);
+					
+					if (transform.position.x > target.position.x){
+						gameObject.GetComponent<SpriteRenderer>().flipX = true;
+					}
 
+					// Ensuite on défini la position à partir de laquelle il doit se tenir du joueur
+					// Le tireur va s'avancer vers le joueur cible pour entrer dans sa range d'attaque
+					// Autrement il va essayer de ne pas dépasser cette range pour se faire taper dessus
+					if (Vector3.Distance(target.GetComponent<Transform>().position, transform.position) > attackRange)
+					{
+						transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x +1, target.position.y, target.position.z), speed * Time.deltaTime);
+					}
+
+					spritesLastName = GetComponent<animationSprite>().currentAnim.sprites.Last().name;
+					spriteCurrentName =  GetComponent<animationSprite>().currentAnim.sprites[GetComponent<animationSprite>().currentSpriteIdx].name;
+
+					if( spritesLastName == spriteCurrentName){
+						attackChoice = "attack1";
+						// Projectiles à rajouter
+					}
+					
+				break;
+
+				// Par défaut, tag = Enemy. Ce qui correspond aux ennemies au corp-à-corp.
+				default:
+					if (Vector3.Distance(Player1.GetComponent<Transform>().position, transform.position) > Vector3.Distance(Player2.GetComponent<Transform>().position, transform.position)){
+					target = Player2.GetComponent<Transform>();
+					}
+					else{
+						target = Player1.GetComponent<Transform>();
+					}
+					transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x +1, target.position.y, target.position.z), speed * Time.deltaTime);
+
+					gameObject.GetComponent<animationSprite>().SetAnimationFromSpeed(speed, true);
+
+					
+					if (transform.position.x > target.position.x){
+						gameObject.GetComponent<SpriteRenderer>().flipX = true;
+					}
+
+					spritesLastName = GetComponent<animationSprite>().currentAnim.sprites.Last().name;
+					spriteCurrentName =  GetComponent<animationSprite>().currentAnim.sprites[GetComponent<animationSprite>().currentSpriteIdx].name;
+
+					if( spritesLastName == spriteCurrentName){
+						attackChoice = "attack" + rnd.Next(1,4);
+					}
+				break;
+			}
 			
-			if (transform.position.x > target.position.x){
-				gameObject.GetComponent<SpriteRenderer>().flipX = true;
-			}
-
-			string spritesLastName = GetComponent<animationSprite>().currentAnim.sprites.Last().name;
-			string spriteCurrentName =  GetComponent<animationSprite>().currentAnim.sprites[GetComponent<animationSprite>().currentSpriteIdx].name;
-
-			if( spritesLastName == spriteCurrentName){
-				attackChoice = "attack" + rnd.Next(1,4);
-			}
 		}
 		/*else
 		{
--- Assets/Scripts/fight.cs
+++ Assets/Scripts/fight.cs
@@ -32,7 +32,7 @@ public class fight : MonoBehaviour {
 
 	// Private 
 	private float accumulateur;
-	private string EnemyType;
+	private string[] enemyType;
 
 	public string CollisonSide;
 
@@ -44,13 +44,14 @@ public class fight : MonoBehaviour {
 	private AudioSource[] sounds;
 	// Use this for initialization
 	void Start () {
-
+		enemyType = new string[3];
 		// Determine le type de personnage qu'il peut frapper
 		if (this.gameObject.tag == "Player"){
-			EnemyType = "Enemy";
+			enemyType[0] = "Enemy";
+			enemyType[1] = "Shooter";
 		}
 		else{
-			EnemyType = "Player";
+			enemyType[0] = "Player";
 		}
 
 		if(cameraMain != null)
@@ -71,9 +72,8 @@ public class fight : MonoBehaviour {
 	void LateUpdate() {
 		// Lorsque la vie arrive à zero fait l'animation de mort
 		if(hp <= 0){
-			if (EnemyType == "Enemy"){
+			if (enemyType[0] == "Enemy"){
 				GetComponent<movePlayer>().IsDead = true;
-
 			}
 			
 			if(gameObject.GetComponent<animationSprite>().currentAnim.name != "die")
@@ -94,7 +94,7 @@ public class fight : MonoBehaviour {
 				gameObject.SetActive(false);
 				
 				// Si c'est un mechant il est le GameObject est détruit
-				if (EnemyType == "Player"){
+				if (enemyType[0] == "Player"){
 					DestroyImmediate(this.gameObject);
 
 					if(isBoss)
@@ -111,7 +111,7 @@ public class fight : MonoBehaviour {
 					}
 				}
 
-				if (EnemyType == "Enemy"){
+				if (enemyType[0] == "Enemy" || enemyType[1] == "Shooter"){
 					
 					Time.timeScale = 0f;
 					sounds.FirstOrDefault(x => x.clip.name.Contains("SoundLevel1")).Stop();
@@ -154,60 +154,64 @@ public class fight : MonoBehaviour {
 		// Permet de n'infliger qu'une seul fois des dégats par coup
         accumulateur += Time.deltaTime;
 
-		// Lorsqu'une collison à lieu avec un enemie
-		if(coll.gameObject.tag == EnemyType){
+		foreach (string enemyType in enemyType)
+		{
+			// Lorsqu'une collison à lieu avec un ennemie
+			if(coll.gameObject.tag == enemyType){
 			
-			string OtherCollSide = coll.gameObject.GetComponent<fight>().CollisonSide;
-			bool OtherflipX = GetComponent<SpriteRenderer>().flipX;
+				string OtherCollSide = coll.gameObject.GetComponent<fight>().CollisonSide;
+				bool OtherflipX = GetComponent<SpriteRenderer>().flipX;
 
-			// Pour chaque attack regarde si le sprite correspond au sprite de degat associé
-			foreach (Attack att in Attacks){
-				if(GetComponent<SpriteRenderer>().sprite == att.SpriteDamage 
-				&& accumulateur > 0.2
-				// Vérifie que les ennemies sont bien face a face pour se frapper
-				&& ((CollisonSide == "R" && OtherCollSide == "L" && OtherflipX == false) 
-				|| (CollisonSide == "L" && OtherCollSide == "R" && OtherflipX == true))
-				)
+				// Pour chaque attack regarde si le sprite correspond au sprite de degat associé
+				foreach (Attack att in Attacks){
+					if(GetComponent<SpriteRenderer>().sprite == att.SpriteDamage 
+					&& accumulateur > 0.2
+					// Vérifie que les ennemies sont bien face a face pour se frapper
+					&& ((CollisonSide == "R" && OtherCollSide == "L" && OtherflipX == false) 
+					|| (CollisonSide == "L" && OtherCollSide == "R" && OtherflipX == true))
+					)
 
-				{	
-					// Inflige des dégats en fonction de l'attaque
-					coll.gameObject.GetComponent<fight>().hp -= att.dammageValue;
+					{	
+						// Inflige des dégats en fonction de l'attaque
+						coll.gameObject.GetComponent<fight>().hp -= att.dammageValue;
 
-					if(att.name == "coup de poing")
-					{
-						AudioSource source = sounds.FirstOrDefault(x => x.clip.name.Contains("punch"));
-						source.PlayOneShot(source.clip);
-					}
-
-					if(att.name == "air kick")
-					{
-						AudioSource source = sounds.FirstOrDefault(x => x.clip.name.Contains("airKick"));
-						source.PlayOneShot(source.clip);
-					}
-
-					if(att.name == "high kick")
-					{
-						AudioSource source = sounds.FirstOrDefault(x => x.clip.name.Contains("HighKick"));
-						source.PlayOneShot(source.clip);
-					}
-
-					// Déclenche l'animation hit
-					if (coll.gameObject.GetComponent<fight>().hp > 0){
-						if(coll.gameObject.GetComponent<animationSprite>().currentAnim.name != "hit")
+						if(att.name == "coup de poing")
 						{
-							cameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "hurt").PlayOneShot(cameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "hurt").clip);
-							coll.gameObject.GetComponent<animationSprite>().ChangeAnimation("hit", true);
+							AudioSource source = sounds.FirstOrDefault(x => x.clip.name.Contains("punch"));
+							source.PlayOneShot(source.clip);
 						}
-					}
 
-					// Si le personnage a une barre de combo la rempli en fonction de l'attaque
-					if (UltBarVar != null){
-						AddHit(att.comboValue);
+						if(att.name == "air kick")
+						{
+							AudioSource source = sounds.FirstOrDefault(x => x.clip.name.Contains("airKick"));
+							source.PlayOneShot(source.clip);
+						}
+
+						if(att.name == "high kick")
+						{
+							AudioSource source = sounds.FirstOrDefault(x => x.clip.name.Contains("HighKick"));
+							source.PlayOneShot(source.clip);
+						}
+
+						// Déclenche l'animation hit
+						if (coll.gameObject.GetComponent<fight>().hp > 0){
+							if(coll.gameObject.GetComponent<animationSprite>().currentAnim.name != "hit")
+							{
+								cameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "hurt").PlayOneShot(cameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "hurt").clip);
+								coll.gameObject.GetComponent<animationSprite>().ChangeAnimation("hit", true);
+							}
+						}
+
+						// Si le personnage a une barre de combo la rempli en fonction de l'attaque
+						if (UltBarVar != null){
+							AddHit(att.comboValue);
+						}
+						accumulateur = 0;
 					}
-					accumulateur = 0;
 				}
 			}
 		}
+		
     }
 
 	private void AddHit(float AddedHit) {
