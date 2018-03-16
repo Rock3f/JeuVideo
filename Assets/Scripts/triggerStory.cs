faf1a9d39c30b2b0c6d3dc8c7ecf2f8be36ebdb5
Rock3f <tanguy.badier@ynov.com>
2018-03-16 08:39:21 +0100

    add sounds intro

--- Assets/Scripts/triggerStory.cs
+++ Assets/Scripts/triggerStory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,7 +8,23 @@ public class triggerStory : MonoBehaviour {
     
     public string action;
     public GameObject[] ActionOnGameObject;
+    public GameObject CameraMain;
 
+    private bool IsEnd = false;
+
+    private void Update() {
+        if(Input.GetButtonDown("Fire1")){
+            if(IsEnd)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }	
+            else
+            {
+                IsEnd = true;
+            }	
+        }
+    }
+    
     private void OnTriggerEnter2D(Collider2D other) {
         switch(action)
         {
@@ -26,9 +43,13 @@ public class triggerStory : MonoBehaviour {
                 ActionOnGameObject[4].GetComponent<movePnjBackGround>().isWaiting = false;
                 ActionOnGameObject[5].GetComponent<movePnjBackGround>().isWaiting = false;
                 ActionOnGameObject[6].GetComponent<movePnjBackGround>().isWaiting = false;
+                CameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "tank").loop = true;
+                CameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "tank").Play();
             }
             break;
             case "scene3":
+                CameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "tank").Stop();
+                CameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "accident").PlayOneShot(CameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "accident").clip);
                 ActionOnGameObject[0].GetComponent<movePnjBackGround>().isWaiting = true;
                 ActionOnGameObject[0].transform.Rotate(0,0,-5.004f, Space.Self); 
                 ActionOnGameObject[1].GetComponent<movePnjBackGround>().positionY = -4f;             
@@ -44,27 +65,22 @@ public class triggerStory : MonoBehaviour {
                     ActionOnGameObject[0].SetActive(false);
                     ActionOnGameObject[1].GetComponent<TrackStory>().target = ActionOnGameObject[2].transform;
                     ActionOnGameObject[2].GetComponent<movePnjBackGround>().isWaiting = false;
+                    CameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "car").Play();
                 }
             break;
             case "scene5":
             if(other.tag != "Player")
             {
+                IsEnd = true;   
+                CameraMain.GetComponents<AudioSource>().FirstOrDefault(x => x.clip.name == "car").Stop();
                 ActionOnGameObject[0].SetActive(false);
-                ActionOnGameObject[1].transform.position = new Vector3(ActionOnGameObject[2].transform.position.x, ActionOnGameObject[1].transform.position.y, ActionOnGameObject[1].transform.position.z);
                 ActionOnGameObject[3].SetActive(true);
                 ActionOnGameObject[4].SetActive(true);
-                ActionOnGameObject[5].SetActive(true);
-                wait();
-                
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                ActionOnGameObject[5].SetActive(true);                
+                ActionOnGameObject[1].GetComponent<TrackStory>().target = ActionOnGameObject[2].transform;             
             }
             break;
 
         }
-    }
-
-    private IEnumerator wait()
-    {
-        yield return new WaitForSeconds(5f);
     }
 }
