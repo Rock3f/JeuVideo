ed02e94334b66e1e9918a7c5edeaf5b33eea400b
Rock3f <tanguy.badier@ynov.com>
2018-04-07 14:47:26 +0200

    fix bug message end

--- Assets/Scripts/BossStoryScript.cs
+++ Assets/Scripts/BossStoryScript.cs
@@ -7,17 +7,28 @@ public class BossStoryScript : MonoBehaviour {
 	// Use this for initializationpublic GameObject cameraMain;
     public GameObject uiToDisplay;
 	public int secondsToWait = 2;
+    private bool IsEnd;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        Time.timeScale = 0f;
-		uiToDisplay.SetActive(true);
-		wait(secondsToWait);
-		uiToDisplay.SetActive(false);
-		Time.timeScale = 1f;
+    private void Update() {
+        if(Input.GetButtonDown("Fire1")){
+            if(IsEnd)
+            {
+                uiToDisplay.SetActive(false);
+		        Time.timeScale = 1f;
+                enabled = false;
+            }	
+            else
+            {
+                IsEnd = true;
+            }
+        }
     }
 
-	IEnumerator wait(int seconds)
-    {
-        yield return new WaitForSeconds(seconds);
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.name == "Black_mamba_boss")
+        {
+            Time.timeScale = 0f;
+		    uiToDisplay.SetActive(true);
+        }
     }
 }
